d6eb137ce35db4f1224a604835f09046788d9e19
caiohrgm <caio.medeiros@ccc.ufcg.edu.br>
2025-08-07 13:39:42 -0300

    add udpate sytem data route

--- project/main.py
+++ project/main.py
@@ -1,16 +1,25 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+import json
 from scrap.scrap import run_scraping
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://www.indigotechnologies.wiki.br"}})
 
 @app.route("/")
 def home():
-    return "API do scraper está no ar!"
+    return "Scraper API is running!"
 
 @app.route("/run-scraping")
 def run():
     data = run_scraping()
     return jsonify({"status": "ok", "results": data})
+
+@app.route("/update-system-data")
+def get_dados():
+    with open("data/systems.json", encoding="utf-8") as f:
+        data = json.load(f)
+    return jsonify(data)
 
 # Adicione isso no final:
 if __name__ == "__main__":
