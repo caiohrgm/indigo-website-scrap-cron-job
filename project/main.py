70576f098f2766c669926807c5814bf944f50f06
caiohrgm <caio.medeiros@ccc.ufcg.edu.br>
2025-08-07 14:07:46 -0300

    fix cors on main

--- project/main.py
+++ project/main.py
@@ -4,7 +4,7 @@ import json
 from scrap.scrap import run_scraping
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://www.indigotechnologies.wiki.br", "http://localhost:5173/"]}})
+CORS(app, resources={r"/*": {"origins": ["https://www.indigotechnologies.wiki.br", "http://localhost:5173"]}})
 
 @app.route("/")
 def home():
